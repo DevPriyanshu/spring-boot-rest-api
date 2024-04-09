a310170a901639c802868fd7951fbc89e38061f4
Priyanshu Yadav <priyansu.ydv.b@gmail.com>
2024-04-09 12:06:42 +0530

    added encoder for password

--- src/main/java/com/crud/api/service/JwtUserDetailsService.java
+++ src/main/java/com/crud/api/service/JwtUserDetailsService.java
@@ -1,20 +1,27 @@
 package com.crud.api.service;
 
-import java.util.ArrayList;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
+
 public class JwtUserDetailsService implements UserDetailsService {
+    @Value("${api.auth.user}")
+    private String authUser;
+    @Value("${api.auth.password}")
+    private String authPassword;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if ("spring-user".equals(username)) {
-            return new User("spring-user", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+        if (authUser.equals(username)) {
+            return new User(authUser, new BCryptPasswordEncoder().encode(authPassword),
                     new ArrayList<>());
         } else {
             throw new UsernameNotFoundException("User not found with username: " + username);
